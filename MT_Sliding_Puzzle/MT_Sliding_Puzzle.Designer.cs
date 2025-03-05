b2c7d1d183b6a24a93d5db2c904a82382a8589ba
Moonika Tamm <117397377+MoonikaTamm@users.noreply.github.com>
2025-03-05 12:28:58 +0200

    Add files via upload

---
+++ MT_Sliding_Puzzle/MT_Sliding_Puzzle.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MT_Sliding_Puzzle
+{
+    partial class MT_Sliding_Puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MT_StartGame = new System.Windows.Forms.Button();
+            this.MT_panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MT_GiveUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MT_StartGame
+            // 
+            this.MT_StartGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.MT_StartGame.Location = new System.Drawing.Point(77, 31);
+            this.MT_StartGame.Name = "MT_StartGame";
+            this.MT_StartGame.Size = new System.Drawing.Size(225, 88);
+            this.MT_StartGame.TabIndex = 0;
+            this.MT_StartGame.Text = "Start Game";
+            this.MT_StartGame.UseVisualStyleBackColor = false;
+            this.MT_StartGame.Click += new System.EventHandler(this.MT_StartGame_Click);
+            // 
+            // MT_panel
+            // 
+            this.MT_panel.Location = new System.Drawing.Point(398, 31);
+            this.MT_panel.Name = "MT_panel";
+            this.MT_panel.Size = new System.Drawing.Size(510, 510);
+            this.MT_panel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 162);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Put the numbers in order:\r\n 1  2  3  4\r\n 5  6  7  8\r\n 9  10 11 12\r\n 13 14 15 X\r\nX" +
+    "-marking the empty slot.\r\n";
+            // 
+            // MT_GiveUp
+            // 
+            this.MT_GiveUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.MT_GiveUp.Location = new System.Drawing.Point(77, 157);
+            this.MT_GiveUp.Name = "MT_GiveUp";
+            this.MT_GiveUp.Size = new System.Drawing.Size(225, 88);
+            this.MT_GiveUp.TabIndex = 3;
+            this.MT_GiveUp.Text = "Give up `-´";
+            this.MT_GiveUp.UseVisualStyleBackColor = false;
+            this.MT_GiveUp.Click += new System.EventHandler(this.MT_GiveUp_Click);
+            // 
+            // MT_Sliding_Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 562);
+            this.Controls.Add(this.MT_GiveUp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MT_panel);
+            this.Controls.Add(this.MT_StartGame);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "MT_Sliding_Puzzle";
+            this.Text = "Sliding Puzzle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MT_StartGame;
+        private System.Windows.Forms.Panel MT_panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MT_GiveUp;
+    }
+}
+
---
+++ MT_Sliding_Puzzle/MT_Sliding_Puzzle.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace MT_Sliding_Puzzle
+{
+    public partial class MT_Sliding_Puzzle : Form
+    {
+        Stopwatch stopwatch;
+        int kx = 4, ky = 4;
+        Button[] MT_buttons;
+        int dx, dy;
+
+        public MT_Sliding_Puzzle()
+        {
+            InitializeComponent();
+            MT_buttons = new Button[kx * ky];
+            dx = MT_panel.Width / kx;
+            dy = MT_panel.Height / ky;
+            MT_makeButtons();
+            stopwatch = new Stopwatch();
+        }
+
+        public void MT_makeButtons()
+        {
+            int k = kx * ky;
+            for (int i = 0; i < k; i++)
+            {
+                MT_buttons[i] = new Button();
+                MT_panel.Controls.Add(MT_buttons[i]);
+                MT_buttons[i].Width = dx;
+                MT_buttons[i].Height = dy;
+                int rida = i / kx;
+                int veerg = i % kx;
+                MT_buttons[i].Left = veerg * dx;
+                MT_buttons[i].Top = rida * dy;
+                MT_buttons[i].Click += new EventHandler(MT_btn_Click);
+                MT_buttons[i].Enabled = false;
+            }
+        }
+
+        private void MT_StartGame_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            int k = kx * ky;
+            int kuhu;
+            for (int i = 0; i < k; i++)
+            {
+                MT_buttons[i].Text = "";
+                MT_buttons[i].Enabled = true;
+            }
+
+            for (int n = 1; n < k; n++)
+            {
+                do
+                {
+
+                    kuhu = rn.Next(0, k);
+
+                } while (MT_buttons[kuhu].Text != "");
+
+                MT_buttons[kuhu].Text = n.ToString();
+
+            }
+            stopwatch.Restart();
+            stopwatch.Start();
+        }
+
+        private void MT_GiveUp_Click(object sender, EventArgs e)
+        {
+            string message = $"Congratulations for giving up!";
+            string caption = "Loser";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, caption, buttons);
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void MT_btn_Click(object sender, EventArgs args)
+        {
+                int n = Array.IndexOf(MT_buttons, (Button)sender);
+
+    if (n % 4 != 3 && MT_buttons[n + 1].Text == "")
+    {
+        (MT_buttons[n].Text, MT_buttons[n + 1].Text) = (MT_buttons[n + 1].Text, MT_buttons[n].Text);
+    }
+    else if (n % 4 != 0 && MT_buttons[n - 1].Text == "")
+    {
+        (MT_buttons[n].Text, MT_buttons[n - 1].Text) = (MT_buttons[n - 1].Text, MT_buttons[n].Text);
+    }
+    else if (n + 4 < 16 && MT_buttons[n + 4].Text == "")
+    {
+        (MT_buttons[n].Text, MT_buttons[n + 4].Text) = (MT_buttons[n + 4].Text, MT_buttons[n].Text);
+    }
+    else if (n - 4 >= 0 && MT_buttons[n - 4].Text == "")
+    {
+        (MT_buttons[n].Text, MT_buttons[n - 4].Text) = (MT_buttons[n - 4].Text, MT_buttons[n].Text);
+    }
+
+    checkForWin();
+}
+
+        public void checkForWin()
+        {
+            int checkedButtons = 0;
+            for (int i = 0; i < MT_buttons.Length - 1; i++)
+            {
+                if (MT_buttons[i].Text != "" && i + 1 == int.Parse(MT_buttons[i].Text))
+                {
+                    checkedButtons++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (checkedButtons == 15)
+            {
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                string formattedTime = $"{ts.Minutes} minute(s) " +
+                                   $"{ts.Seconds} second(s)";
+                string message = $"Congratulations. You have solved this puzzle in {formattedTime}!";
+                string caption = "Winner";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, caption, buttons);
+
+            }
+        }
+    }
+}
